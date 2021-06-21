b17e7dddc5eed80512e9c131b8a20dbc2891d2f4
Christian Parker <christianparker512@gmail.com>
2021-06-21 16:54:47 -0500

    server requests sending things to the browser

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) =>{
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    //set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write('<head><link rel="stylesheet" href="#"></head>');
+    res.write('<h1>Hookem Horns</h1>');
+    res.write('<h3>"The Eyes of Texas Are Upon you!"</h3>')
+    res.end();
 });
 server.listen(3000, 'localhost', ()=>{
     console.log("Listening requests on port 3000")

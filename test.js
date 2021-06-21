2748a964eb8d23c879c9e63e89fcca98afd47b8f
Christian Parker <christianparker512@gmail.com>
2021-06-21 14:11:44 -0500

    node test page with javascript

--- test.js
+++ test.js
@@ -1,2 +1,9 @@
-const name = 'christian';
-console.log(name);
+const name = 'abby';
+const name2 = 'christian';
+const name3 = 'lisa';
+console.log(name, name2, name3);
+
+const greet = (name) => {
+    console.log(`hello, ${name}`)
+}
+console.log(greet('Christian'));

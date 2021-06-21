ea8b0617b10738a6deb27471570c8f728712ad1f
Christian Parker <christianparker512@gmail.com>
2021-06-21 16:23:01 -0500

    readable streams and writable streams

--- streams.js
+++ streams.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 
-const readStream = fs.createReadStream('./docs/blog3.txt');
-
+const readStream = fs.createReadStream('./docs/blog3.txt', {encoding: 'utf8'});
+const writeStream = fs.createWriteStream('./docs/blog4.txt');
 readStream.on('data', (chunk) => {
-
+    console.log('---- Horns up!-----');
+    console.log(chunk);
+    writeStream.write('\nNewChunk\n');
+    writeStream.write('chunk');
 })

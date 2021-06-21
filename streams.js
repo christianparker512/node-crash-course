5d6c587c48d1c7e344d27e0c913d81dcdebf033f
Christian Parker <christianparker512@gmail.com>
2021-06-21 16:24:35 -0500

    read and writing data using streams

--- streams.js
+++ streams.js
@@ -2,9 +2,12 @@ const fs = require('fs');
 
 const readStream = fs.createReadStream('./docs/blog3.txt', {encoding: 'utf8'});
 const writeStream = fs.createWriteStream('./docs/blog4.txt');
-readStream.on('data', (chunk) => {
-    console.log('---- Horns up!-----');
-    console.log(chunk);
-    writeStream.write('\nNewChunk\n');
-    writeStream.write('chunk');
-})
+// readStream.on('data', (chunk) => {
+//     console.log('---- Horns up!-----');
+//     console.log(chunk);
+//     writeStream.write('\nNewChunk\n');
+//     writeStream.write('chunk');
+// })
+
+//
+readStream.pipe(writeStream);

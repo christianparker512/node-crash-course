8be4d68aa0aa3a727c228b6e8f4dfdb0ffff5e72
Christian Parker <christianparker512@gmail.com>
2021-06-21 14:48:49 -0500

    exporting information from one file to another

---
+++ people.js
@@ -0,0 +1,9 @@
+const people = ['christian', 'lisa', 'abby', 'quincy', 'baxter'];
+const ages = [50,56,19,13,12];
+console.log(people);
+
+module.exports = 'hello'
+
+module.exports = {
+    people, ages
+};

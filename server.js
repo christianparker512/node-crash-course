7990680c6710795900d1adf09e2dd091e4fbae5d
Christian Parker <christianparker512@gmail.com>
2021-06-22 09:22:30 -0500

    view directory, view, and changing the server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) =>{
     console.log(req.url, req.method);
@@ -6,10 +7,17 @@ const server = http.createServer((req, res) =>{
     //set header content type
     res.setHeader('Content-Type', 'text/html');
 
-    res.write('<head><link rel="stylesheet" href="#"></head>');
-    res.write('<h1>Hookem Horns</h1>');
-    res.write('<h3>"The Eyes of Texas Are Upon you!"</h3>')
-    res.end();
+    // send an html file
+    fs(readFile('./views/index.html', (err, data) =>{
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    }))
+
 });
 server.listen(3000, 'localhost', ()=>{
     console.log("Listening requests on port 3000")

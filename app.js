34263fb577a34af4c6c89ea789a73bd47d1f8242
Christian Parker <christianparker512@gmail.com>
2021-06-22 14:32:06 -0500

    create.ejs

--- app.js
+++ app.js
@@ -3,23 +3,31 @@ const express = require('express');
 //express app
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs');
+
 //listen for requests
 app.listen(4000);
 
 app.get('/', (req,res) =>{
     // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', {root: __dirname});
+    // res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index');
 });
     app.get('/about', (req,res) =>{
         // res.send('<p>about page</p>');
-        res.sendFile('./views/about.html', {root: __dirname});
+        res.render('about', {root: __dirname});
     //redirects
         app.get('/about-us', (req,res)=>{
-            res.redirect('/about');
+            res.render('/about');
+        });
+        app.get('/blogs/create', (req,res) =>{
+            res.render('/create');
         });
         //404 middleware functions in express
         app.use((req,res) => {
-            res.status(404).sendFile('./views/404.html', {root: __dirname});
-        } )
+            // res.status(404).sendFile('./views/404.html', {root: __dirname});
+            res.status(404).render('404');
+        });
 
 });
